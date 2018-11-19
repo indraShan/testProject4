914e3bf21dda83522e08943042587b421546f951
indrajit <indrashanbhag@gmail.com>
2018-11-19 01:53:27 -0500

    Creted project structure

---
+++ lets_make_money/lib/lets_make_money.ex
@@ -0,0 +1,18 @@
+defmodule Starter do
+  @moduledoc """
+  Documentation for Starter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Starter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
